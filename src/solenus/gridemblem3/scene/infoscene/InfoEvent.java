64247c5d612e247803152276e74d6f53143e93e2
CSDragon <CSDragon31@hotmail.com>
2016-03-11 03:56:20 -0800

    Created the infomenu generic menu

--- src/solenus/gridemblem3/scene/infoscene/InfoEvent.java
+++ src/solenus/gridemblem3/scene/infoscene/InfoEvent.java
@@ -39,5 +39,69 @@ public class InfoEvent
       
         in.readLine();
     }
+
+    /**
+     * @return the name
+     */
+    public String getName() 
+    {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    /**
+     * @return the fileName
+     */
+    public String getFileName() 
+    {
+        return fileName;
+    }
+
+    /**
+     * @param fileName the fileName to set
+     */
+    public void setFileName(String fileName) 
+    {
+        this.fileName = fileName;
+    }
+
+    /**
+     * @return the weaponRewards
+     */
+    public ArrayList<Weapon> getWeaponRewards() 
+    {
+        return weaponRewards;
+    }
+
+    /**
+     * @param weaponRewards the weaponRewards to set
+     */
+    public void setWeaponRewards(ArrayList<Weapon> weaponRewards) 
+    {
+        this.weaponRewards = weaponRewards;
+    }
+
+    /**
+     * @return the itemRewards
+     */
+    public ArrayList<Usable> getItemRewards() 
+    {
+        return itemRewards;
+    }
+
+    /**
+     * @param itemRewards the itemRewards to set
+     */
+    public void setItemRewards(ArrayList<Usable> itemRewards) 
+    {
+        this.itemRewards = itemRewards;
+    }
     
 }
---
+++ src/solenus/gridemblem3/scene/infoscene/InfoMenu.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package solenus.gridemblem3.scene.infoscene;
+
+import java.util.ArrayList;
+import solenus.gridemblem3.ui.menu.GenericMenu;
+
+/**
+ *
+ * @author Chris
+ */
+public class InfoMenu extends GenericMenu
+{
+    private ArrayList<InfoEvent> events;
+    
+    public InfoMenu(ArrayList<InfoEvent> ie)
+    {
+        super(eventListNameArray(ie));
+        ie = events;
+    }
+    
+    public static String[] eventListNameArray(ArrayList<InfoEvent> ie)
+    {
+        String[] ret = new String[ie.size()];
+        
+        for(int i = 0; i<ie.size(); i++)
+            ret[i] = ie.get(i).getName();
+        
+        return ret;
+    }
+}
